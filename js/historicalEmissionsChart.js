08146635ee6807d2adf5d359637b06ae959a166a
Feng <afeng@urban.org>
2018-09-22 14:01:05 -0400

    Remove commas from tick format

--- js/historicalEmissionsChart.js
+++ js/historicalEmissionsChart.js
@@ -44,7 +44,7 @@
 
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(xScale));
+            .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
 
         svg.append("g")
             .call(d3.axisLeft(yScale))
