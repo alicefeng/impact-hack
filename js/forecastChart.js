79c0176a9115f7e727dc99c9489f705fd4a918f7
Feng <afeng@urban.org>
2018-09-22 16:29:53 -0400

    Fix margins

--- js/forecastChart.js
+++ js/forecastChart.js
@@ -24,8 +24,8 @@
 
     var svg = d3.select("#forecastChart")
         .append("svg")
-        .attr("width", width + margin.top + margin.bottom)
-        .attr("height", height + margin.left + margin.right)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
