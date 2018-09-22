c4c819d46883e9365a35dacc652a725d3a50a564
Feng <afeng@urban.org>
2018-09-22 10:54:46 -0400

    Implement scrollytelling transitions with bubble chart

--- js/bubbleChart.js
+++ js/bubbleChart.js
@@ -37,7 +37,13 @@ d3.csv("data/paris_groups.csv",function(d) {
       //     d.class = id.slice(i + 1);
       //   }
       // });
-console.log(pack(root).leaves());
+
+  createBubbleChart(root);
+});
+
+
+function createBubbleChart(root) {
+
   var node = svg.selectAll(".node")
     .data(pack(root).leaves())
     .enter().append("g")
@@ -46,32 +52,32 @@ console.log(pack(root).leaves());
 
   node.append("circle")
       // .attr("id", function(d) { return d.data.Country; })
+      .attr("class", "hidden")
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) { return d.r; })
-      .style("fill", "#1696d2");
+      .attr("r", function(d) { return d.r; });
 
   node.append("text")
-    .attr("class", "countryNameLabel")
+    .attr("class", "countryNameLabel hidden")
     .attr("x", function(d) { return d.x; })
     .attr("y", function(d) { return d.y; })
     .attr("dy", ".35em")
     .text(function(d) { return d.r > 40 ? d.data.country : ""; });
 
-  // node.append("clipPath")
-  //     .attr("id", function(d) { return "clip-" + d.id; })
-  //   .append("use")
-  //     .attr("xlink:href", function(d) { return "#" + d.id; });
+}
 
-  // node.append("text")
-  //     .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-  //   .selectAll("tspan")
-  //   .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-  //   .enter().append("tspan")
-  //     .attr("x", 0)
-  //     .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-  //     .text(function(d) { return d; });
-
-  // node.append("title")
-  //     .text(function(d) { return d.id + "\n" + format(d.value); });
-});
+function updateGraph(step) {
+    console.log("triggered!", step);
+    if(step === 0) {
+        d3.selectAll(".country.group1 circle").classed("hidden", false);
+        d3.selectAll(".country.group1 .countryNameLabel.hidden").classed("hidden", false);
+    }
+    else if(step === 1) {
+        d3.selectAll(".country.group2 circle").classed("hidden", false);
+        d3.selectAll(".country.group2 .countryNameLabel.hidden").classed("hidden", false);
+    }
+    else if(step === 2) {
+        d3.selectAll(".country.group3 circle").classed("hidden", false);
+        d3.selectAll(".country.group3 .countryNameLabel.hidden").classed("hidden", false);
+    }
+}
